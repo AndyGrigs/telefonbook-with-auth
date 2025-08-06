f272004a0b432766ebbf2ab193fd850fdd35d61b
Andrii Grygorov <97991761+AndyGrigs@users.noreply.github.com>
2025-08-06 07:39:58 +0200

    Update RegisterPage.jsx

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+// src/pages/RegisterPage.jsx
+import React from 'react';
+import { Link } from 'react-router-dom';
+import RegisterForm from '../components/RegisterForm';
 
 const RegisterPage = () => {
   return (
-    <div>RegisterPage</div>
-  )
-}
-
-export default RegisterPage
+    <div className="max-w-md mx-auto px-4">
+      <RegisterForm />
+      <p className="text-center mt-4 text-sm text-gray-600">
+        Already have an account?{' '}
+        <Link to="/login" className="text-blue-600 hover:text-blue-800">
+          Sign in here
+        </Link>
+      </p>
+    </div>
+  );
+};
