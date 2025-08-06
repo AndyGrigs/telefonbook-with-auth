9390e044ae3ba90bf685ddb1bf4a18f0de15ba48
Andrii Grygorov <97991761+AndyGrigs@users.noreply.github.com>
2025-08-06 07:41:25 +0200

    Update ContactsPage.jsx

--- src/pages/ContactsPage.jsx
+++ src/pages/ContactsPage.jsx
@@ -1,9 +1,42 @@
-import React from 'react'
+
+// src/pages/ContactsPage.jsx
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import ContactForm from "../components/ContactForm";
+import SearchBox from "../components/SearchBox";
+import ContactList from "../components/ContactList";
+import { fetchContacts } from "../redux/contactsOps";
+import { selectError, selectLoading } from "../redux/contactsSlice";
 
 const ContactsPage = () => {
-  return (
-    <div>ContactsPage</div>
-  )
-}
+  const dispatch = useDispatch();
+  const isLoading = useSelector(selectLoading);
+  const error = useSelector(selectError);
 
-export default ContactsPage
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
+
+  return (
+    <div className="max-w-md mx-auto px-4">
+      {error && (
+        <div className="mb-4 p-4 bg-red-100 text-red-700 rounded-md">
+          {error}
+        </div>
+      )}
+
+      <ContactForm />
+      <SearchBox />
+      
+      {isLoading && (
+        <div className="flex justify-center my-4">
+          <div className="animate-spin rounded-full h-8 w-8 border-b-2 border-blue-600"></div>
+        </div>
+      )}
+      
+      <ContactList />
+    </div>
+  );
+};
+
+export default ContactsPage;
