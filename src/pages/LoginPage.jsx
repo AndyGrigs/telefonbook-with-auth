0849ac88f6ded431de6ade73a87bd1d0af5d769e
Andrii Grygorov <97991761+AndyGrigs@users.noreply.github.com>
2025-08-06 07:38:35 +0200

    Update LoginPage.jsx

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+// src/pages/LoginPage.jsx
+import React from 'react';
+import { Link } from 'react-router-dom';
+import LoginForm from '../components/LoginForm';
 
 const LoginPage = () => {
   return (
-    <div>LoginPage</div>
-  )
-}
+    <div className="max-w-md mx-auto px-4">
+      <LoginForm />
+      <p className="text-center mt-4 text-sm text-gray-600">
+        Don't have an account?{' '}
+        <Link to="/register" className="text-blue-600 hover:text-blue-800">
+          Sign up here
+        </Link>
+      </p>
+    </div>
+  );
+};
 
-export default LoginPage
+export default LoginPage;
